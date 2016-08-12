dc147eb27a868ba3f77d1510c97bfe70f4b8e0b6
root <root@ubuntu.members.linode.com>
2016-08-12 22:23:08 +0000

    updatepage

--- updateagQU0dxEfsH3Zm4PWWKwgSqUnQXYEdYXSxv4kppEbIbjGL791NJGvtNQQNfAcbSTHL92h8FxCjsUSpyOGzmYsxkjOb1szAwuwE7IJBgKb5k8kSiCMDjU1HTsw68sJfXKrwV76EOcldwiUPVK2xTLwnFJiO7LpiOmeKD6eZbjCTE9K87Rki9tq6MyPTUSHGGOEC96fmLr22tEYFEJsjWD0mKZt15zhVAcREPzPjlX4s.php
+++ updateagQU0dxEfsH3Zm4PWWKwgSqUnQXYEdYXSxv4kppEbIbjGL791NJGvtNQQNfAcbSTHL92h8FxCjsUSpyOGzmYsxkjOb1szAwuwE7IJBgKb5k8kSiCMDjU1HTsw68sJfXKrwV76EOcldwiUPVK2xTLwnFJiO7LpiOmeKD6eZbjCTE9K87Rki9tq6MyPTUSHGGOEC96fmLr22tEYFEJsjWD0mKZt15zhVAcREPzPjlX4s.php
@@ -2,8 +2,19 @@
 <html>
 <head>
 <script>
-function gotolink(link) {
-link =link.concat(document.getElementById("n").value);
+function gotolink0(link) {
+link = link.concat("?n=")
+link =link.concat(document.getElementById("SHOWS").value);
+window.location = link;
+}
+function gotolink1(link) {
+link = link.concat("?n=");
+link =link.concat(document.getElementById("CONTENT").value);
+window.location = link;
+}
+function gotolink2(link) {
+link = link.concat("?n=");
+link =link.concat(document.getElementById("STORE").value);
 window.location = link;
 }
 </script>
@@ -13,20 +24,20 @@ window.location = link;
 <form action="showschangekFue7PD3YhObQbDS3DwLQyzKSJfaHO7avWorK2cIoaotYNlxeDKfYVfB740UVPzuUfqEOYc7kPhCH1hZ7slcid198Nq2pN4T119KKkEQKOBhNz2MZW3UswjElHbAWqPsLOg4nhH6h2FvzAkKWWwfiIVLRB2BDYVwrDFsboaYC6MaP81yqmkrQRz8YeZaakJ0vmVMOWsOXHQWXHBBgzfOVjvNGEbuTQMjJXUVl.php">
     <input type="submit" value="Change/Remove Content In Shows" />
 </form>
-<input type="number" id="n" step="1" min="1" value="1"/>
-<button onclick="gotolink(showsaddwGsffKUF5QD25KzMDPoJ7ByNVOTidjH8y3XsssHjlQeJv7b5YlZegWS8xBZ4zPNHveDh8RGQNjmEuOVwoYfVepupsoFw2pP4q2VzDMm5sTA4ayO6ZrAanVp9ddeAy5r31in1wQf0A6CrjVvWYwj6h73pOc51JkfFH0YbyoLzKynTDw2f3chjlx6UVU5xFrb7c0B3kiSEumNEErkn53kmYpwSVYJlcZP9BqxJ6yt5.php)" type="submit">Add Shows</button>
+<input type="number" id="SHOWS" step="1" min="1" value="1"/>
+<button onclick="gotolink0(showsaddwGsffKUF5QD25KzMDPoJ7ByNVOTidjH8y3XsssHjlQeJv7b5YlZegWS8xBZ4zPNHveDh8RGQNjmEuOVwoYfVepupsoFw2pP4q2VzDMm5sTA4ayO6ZrAanVp9ddeAy5r31in1wQf0A6CrjVvWYwj6h73pOc51JkfFH0YbyoLzKynTDw2f3chjlx6UVU5xFrb7c0B3kiSEumNEErkn53kmYpwSVYJlcZP9BqxJ6yt5.php)" type="submit">Add Shows</button>
 <form action="contentchangecOBUW3honLyHsIU6CkXZjvIISlrL4wSdMVB3KejXE2Zg0qzGl3WhMTHpmyrUHIim0rgFbWZvFw5HHuFbGKkCl7aKYftK3u1u4bR2vZylMZRwImzoCg42CBP54uxGkoC3auj8z6EvA3gq51Qqc9rpkWzqXpOiJlblxTVOn7BrSQPSfA2I08nXs1DJNkIx9NsacYOyAJNZEjyXNupCCg8TN7qrrXLpkoiOInq.php">
     <input type="submit" value="Change/Remove Content From Media" />
 </form>
-<input type="number" id="n" step="1" min="1" value="1"/>
-<button onclick="gotolink(contentaddqI43Mj74lYJgXVyeIvrLgeCayrep7ns05APQjG3zLVDVYeP3J50J5bidx22xpjBerwsPFch1hKLZepVmTIvUAwtXIlUwmnvn23c1zS60UmTvJnX27hH1sYyXdOVUgChjhcQALmcaLGLsMQ1v5DiWKapYPywzqnIwAhb9mw98Zj5A8EhAr962e3oZGawm0UbH1YowduVpay5ov3FAcCbKc6OO1U386KPWNUbeCK.php)" type="submit">Add Content to Media</button>
+<input type="number" id="CONTENT" step="1" min="1" value="1"/>
+<button onclick="gotolink1(contentaddqI43Mj74lYJgXVyeIvrLgeCayrep7ns05APQjG3zLVDVYeP3J50J5bidx22xpjBerwsPFch1hKLZepVmTIvUAwtXIlUwmnvn23c1zS60UmTvJnX27hH1sYyXdOVUgChjhcQALmcaLGLsMQ1v5DiWKapYPywzqnIwAhb9mw98Zj5A8EhAr962e3oZGawm0UbH1YowduVpay5ov3FAcCbKc6OO1U386KPWNUbeCK.php)" type="submit">Add Content to Media</button>
 <form action="biochangeEc0nvK3cTtpV5zAHerfDciR6v7hQZEwXoEGYFWmAgbpXKfafKxfHDRL07og96XqTPmg4Emg2tOX2YRt0M7GLZS72ts5Xqt2AuNRl2wM91nubVacrT6dACbICoaj2ku9lFQE72RtKqkxZut6aOpqoIgABLMkJ727Osi6QQBN54E6M3EhNoVarudk1cHBM4HxDmizOwYxc1WGU4JlshLIuFlnSC0z937A8SWsy35Q.php">
     <input type="submit" value="Change Bio" />
 </form>
 <form action="storechangeKymyOJJz7dGyize5mWNseLfJD67NlQs4cX1t1RhwUsdFiBk0mA99OWIBGoTqeicqYN9sbEsUgiODy26PB4ALTGWAWdDnTqBKE6kvY6HaUSbC8rcMhB3jR6OWyq1XulrPBTyLaC8zyIdCIOYSXcAiDmob2zD28pZpWUSMwwxHyYmFpqnVZrBtnDhAQ2ICqw3VgFRo4bH1xyBmfMHLe4yXeVxv5mGeytlXj0VYD.php">
     <input type="submit" value="Change/Remove Content In The Store" />
 </form>
-<input type="number" id="n" step="1" min="1" value="1"/>
-<button onclick="gotolink(storeaddozKi587ZuLBRbkkip4pF8PcECk6etirL2UstFSdbljtw5YpMnlmDMOEOlT3G6nr2MieU9EpzwOr4Vm9IcLZZjjMA1DH5ljS4TuRENNw8wYrXmTDDMiAozgOpcl4XNsCfYukiVpToIlj9mFNbeizK280sQQ54pkNfiOZ8JqExbkgQdXrL2u7ceDnmiC3e0FFxnO4hWhYMIMA9h0S3yNduuHtKBUycWrH6jvTgePQx.php)" type="submit">Add Products to the Store</button>
+<input type="number" id="STORE" step="1" min="1" value="1"/>
+<button onclick="gotolink2(storeaddozKi587ZuLBRbkkip4pF8PcECk6etirL2UstFSdbljtw5YpMnlmDMOEOlT3G6nr2MieU9EpzwOr4Vm9IcLZZjjMA1DH5ljS4TuRENNw8wYrXmTDDMiAozgOpcl4XNsCfYukiVpToIlj9mFNbeizK280sQQ54pkNfiOZ8JqExbkgQdXrL2u7ceDnmiC3e0FFxnO4hWhYMIMA9h0S3yNduuHtKBUycWrH6jvTgePQx.php)" type="submit">Add Products to the Store</button>
 </body>
 </html>
